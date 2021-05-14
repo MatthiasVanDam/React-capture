9c273e112f9f3818b335978cb987e752ff8619d1
MatthiasVanDam <matthias.1991@live.be>
2021-05-14 15:42:21 +0200

    ourWork anime

--- src/Animation.js
+++ src/Animation.js
@@ -6,11 +6,15 @@ export const pageAnimation = {
   show: {
     opacity: 1,
     y: 0,
-    transition: { duration: 1, when: "beforeChildren", staggerChildren: 0.25 },
+    transition: {
+      duration: 0.5,
+      when: "beforeChildren",
+      staggerChildren: 0.25,
+    },
   },
   exit: {
     opacity: 0,
-    y: 300,
+
     transition: { duration: 0.5 },
   },
 };
@@ -31,4 +35,25 @@ export const photoAnime = {
     opacity: 1,
     transition: { duration: 0.75 },
   },
+};
+
+export const lineAnime = {
+  hidden: { width: "0%" },
+  show: {
+    width: "100%",
+    transition: { duration: 0.75 },
+  },
+};
+
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: { ease: "easeOut", duration: 1 },
+  },
+};
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
 };
